5bd7672bbeeeafe24745d91d43cb9f91e2f9a6f2
Serena-Dong <serenadong3159@icloud.com>
2023-03-10 12:35:19 +0100

    index and show structure

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,23 +1,34 @@
 @extends('layouts.app')
 
+@section('title', 'Projects')
+
 @section('content')
 <div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('My Projects') }}
-    </h2>
+    <div class="d-flex align-items-center justify-content-between">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('My Projects') }}
+        </h2>
+        <div class="button">
+            <a class="btn btn-primary" href="{{route('admin.projects.create')}}">Add</a>
+        </div>
+    </div>
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header text-center">{{ __('P O R T F O L I O') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+                <div class="card-body d-flex flex-wrap justify-content-center">
+
+                    @foreach ($projects as $project)
+
+                    <div class="card col-3 m-3 text-center">
+                        <img src="{{$project['image_url']}}" alt="">
+                        <a href="{{route('admin.projects.show', $project['id'])}}"><h5 class="m-3" style="text-transform: uppercase;">{{$project['title']}}</h5></a>
                     </div>
-                    @endif
+                    
+                    @endforeach
 
-                    {{ __('You are logged in!') }}
+                    
                 </div>
             </div>
         </div>
---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('title', $project['name'])
+
+
+@section('content')
+<div class="container">
+    <h2 class="fs-4 text-secondary my-4">
+        {{ __('My Projects') }}
+    </h2>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-body d-flex flex-wrap justify-content-center">
+                    <div class="col-4 p-3">
+                        <img src="{{$project['image_url']}}" alt="">
+                    </div>
+                    <div class="col-6">
+                        <div class="text">
+                            <h2 class="mb-4" style="text-transform: uppercase;">{{$project['title']}}</h2>
+                            <p>{{$project['description']}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="container my-4">
+    <div class="d-flex justify-content-between">
+        <div class="col">
+            <a class="btn btn-success" href="{{route('admin.projects.index')}}"><i class="fa-solid fa-chevron-left"></i> Go Back</a>
+        </div>
+
+        <div class="col d-flex justify-content-end">
+            <a class="btn btn-primary mx-2" href="{{route('admin.projects.edit', $project->id)}}">Edit</a>
+            
+            <form class="delete-form" action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" data-project-name="{{ $project->name }}">
+                @method('DELETE')
+                @csrf
+                <button class="btn btn-small btn-danger me-2"><i class="fa-solid fa-trash"></i> Delete</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+</div>
+@endsection
