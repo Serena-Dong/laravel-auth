84508e2241508d9eabdbc783e51b38e1599dcffe
Serena-Dong <serenadong3159@icloud.com>
2023-03-10 12:34:52 +0100

    crud funtions

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Project;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
 
 class ProjectController extends Controller
 {
@@ -12,7 +15,9 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view('admin.projects.index');
+        $projects = Project::all();
+
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
@@ -20,7 +25,9 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        $project = new Project();
+
+        return view('admin.projects.create', compact('project'));
     }
 
     /**
@@ -28,38 +35,62 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|unique:projects',
+            'project_url' => 'required|url',
+            'image_url' => 'url|nullable',
+            'description' => 'required|string'
+        ]);
+
+        $data = $request->all();
+
+        $new_project = new Project();
+        $new_project->fill($data);
+        $new_project->save();
+
+        return to_route('admin.projects.show', $new_project->id);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Project $project)
     {
-        //
+        return view('admin.projects.show', compact('project'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Project $project)
     {
-        //
+        return view('admin.projects.edit', compact('project'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Project $project)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|unique:projects',
+            'project_url' => 'required|url',
+            'image_url' => 'url|nullable',
+            'description' => 'required|string'
+        ]);
+
+        $data = $request->all();
+        $project->fill($data);
+        $project->save();
+        return to_route('admin.projects.show', $project->id);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Project $project)
     {
-        //
+        $project->delete();
+        return to_route('admin.projects.index');
     }
 }
