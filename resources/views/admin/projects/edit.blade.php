2b7f896102fef0704a9e0cb47922c75d77d6cc12
Serena-Dong <serenadong3159@icloud.com>
2023-03-10 12:35:08 +0100

    edit structure

---
+++ resources/views/admin/projects/edit.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('title', 'Edit ' . $project['name'])
+
+@section('content')
+
+<div class="container mt-5 p-5">
+
+    <div class="card">                
+        <div class="card-body d-flex flex-wrap justify-content-center">
+            
+            <form action="{{ route('admin.projects.update', $project->id) }}" method="POST">
+                <h3 class="text-center my-3">Edit the Info</h3>
+                @csrf
+                @method('PUT')
+                <div class="row ">
+                    <div class="mb-3 col-6 px-5">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="title" name="title" value="{{ old('title', $project->title) }}">
+                    </div>
+                    <div class="mb-3 col-6 px-5">
+                        <label for="project_url" class="form-label">Project URL</label>
+                        <input type="url" class="form-control" id="project_url" name="project_url" value="{{ old('project_url', $project->project_url) }}">
+                    </div>
+                    <div class="mb-3 col-6 px-5">
+                        <label for="image_url" class="form-label">Image URL</label>
+                        <input type="url" class="form-control" id="image_url" name="image_url" value="{{ old('image_url', $project->image_url) }}">
+                    </div>
+                    <div class="mb-3 col-6 px-5">
+                        <label for="description" class="form-label">Description</label>
+                        <input type="text" class="form-control" id="description" name="description" value="{{ old('description', $project->description) }}">
+                    </div>
+                </div>
+
+                <div class="buttons d-flex justify-content-between">
+                    <a class="btn btn-primary mx-5 my-2" href="{{route('admin.projects.index')}}"><i class="fa-solid fa-chevron-left"></i> Go Back</a>
+                    <button type="submit" class="btn btn-success mx-5 my-2"><i class="fa-regular fa-floppy-disk"></i> Save</button>
+                </div>
+
+            </form>
+            
+            
+        </div>
+    </div>
+
+</div>
+@endsection
