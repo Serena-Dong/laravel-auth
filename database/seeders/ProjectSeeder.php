215e45f5dfa2bfdc67aca045a7777162822c76da
Serena-Dong <serenadong3159@icloud.com>
2023-03-10 12:24:20 +0100

    changes in the seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -16,12 +16,12 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for ($i = 0; $i < 5; $i++) {
+        for ($i = 0; $i < 6; $i++) {
             $project = new Project();
             $project->title = $faker->words(3, true);
             $project->project_url = $faker->url();
             $project->image_url = $faker->imageUrl(150, 150);
-            $project->description = $faker->text(50);
+            $project->description = $faker->paragraphs(2, true);
             $project->save();
         }
     }
